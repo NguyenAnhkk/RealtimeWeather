888d1e6557be69d32b3a3adc1f5f3a2f365851a3
Anh Nguyễn <nguyenanhcry@gmail.com>
2025-09-13 09:51:14 +0700

    fetch data

--- app/src/main/java/com/example/realtimeweather/WeatherPage.kt
+++ app/src/main/java/com/example/realtimeweather/WeatherPage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -14,16 +15,19 @@ import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.realtimeweather.api.NetworkResponse
 
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel) {
     var city by remember { mutableStateOf("") }
+    val weatherResult = viewModel.weatherResult.observeAsState()
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,6 +51,14 @@ fun WeatherPage(viewModel: WeatherViewModel) {
                 Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
 
             }
+        }
+        when(val result = weatherResult.value){
+            is NetworkResponse.Error -> {Text(text = result.errorMessage)}
+            NetworkResponse.Loading -> CircularProgressIndicator()
+            is NetworkResponse.Success<*> -> {
+                Text(text = result.data.toString())
+            }
+            null -> TODO()
         }
     }
 
